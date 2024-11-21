381e3613afbbe70cee613ad105ef3f8b876ed9bf
chrisitopherus <jochchristopher@gmail.com>
2024-11-21 03:11:44 +0100

    defined the event args for the cursor updates

--- Minesweeper/Events/CursorMovedEventArgs.cs
+++ Minesweeper/Events/CursorMovedEventArgs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Minesweeper.Board;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +8,15 @@ namespace Minesweeper.Events;
 
 public class CursorMovedEventArgs : EventArgs
 {
+    private BoardPosition previousPosition;
+    private BoardPosition newPosition;
+    public CursorMovedEventArgs(BoardPosition previousPosition, BoardPosition newPosition)
+    {
+        this.previousPosition = previousPosition;
+        this.newPosition = newPosition;
+    }
+
+    public BoardPosition PreviousPosition => this.previousPosition;
+
+    public BoardPosition NewPosition => this.newPosition;
 }
