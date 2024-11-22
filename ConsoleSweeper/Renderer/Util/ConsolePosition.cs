0e68d18b1d80ba48fa79d1a35ec97e1ec08fc7a1
chrisitopherus <jochchristopher@gmail.com>
2024-11-22 14:22:54 +0100

    added ConsolePosition Class

---
+++ ConsoleSweeper/Renderer/Util/ConsolePosition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSweeper.Renderer.Util;
+
+public struct ConsolePosition(int x, int y)
+{
+    public int X { get; } = x;
+    public int Y { get; } = y;
+}
