a08bbcdf7cf50693cb0defe507cf127f595a6ab4
chrisitopherus <jochchristopher@gmail.com>
2024-12-06 23:32:00 +0100

    added menu events

---
+++ ConsoleSweeper/Menu/Events/OnMenuClosedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSweeper.Menu.Events;
+
+public class OnMenuClosedEventArgs : EventArgs
+{
+    public OnMenuClosedEventArgs()
+    {
+    }
+}
---
+++ ConsoleSweeper/Menu/Events/OnMenuIndexChangedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSweeper.Menu.Events;
+
+public class OnMenuIndexChangedEventArgs : EventArgs
+{
+    private readonly int prevIndex;
+    private readonly int nextIndex;
+    public OnMenuIndexChangedEventArgs(int prevIndex, int newIndex)
+    {
+        this.prevIndex = prevIndex;
+        this.nextIndex = newIndex;
+    }
+
+    public int PrevIndex => this.prevIndex;
+    public int NextIndex => this.nextIndex;
+}
